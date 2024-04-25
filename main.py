73a6e1fd5d95e6dbc4ecc80c45bb1fe8468e81de
tk0314init <tktktk.0283@gmail.com>
2024-04-25 14:10:37 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from argparse import Namespace
+import time
+import os
+import sys
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+import torchvision.transforms as transforms
+
+from encoder4editing.utils.common import tensor2im
+from encoder4editing.models.psp import pSp
+
+
+def main():
+    print("hello")
+    
+    
+    
+if __name__ == "__main__":
+    main()
